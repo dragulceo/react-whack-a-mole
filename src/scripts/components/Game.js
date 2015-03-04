ad4c2d952ad94bbc1c0c70e125e3cf0f8c184c4e
Tavi B <dragulceo@Tavis-MBP.lan>
2015-03-04 05:03:35 +0200

    Add Time component into Game

--- src/scripts/components/Game.js
+++ src/scripts/components/Game.js
@@ -5,6 +5,7 @@ var React = require('react/addons');
 require('../../styles/Game.less');
 
 var Score = require('./Score');
+var Time = require('./Time');
 var Board = require('./Board');
 var GameStore = require('../stores/game');
 var GameActions = require('../actions/game');
@@ -25,6 +26,9 @@ var Game = React.createClass({
 	startGameClicked() {
 		GameActions.start();
 	},
+	stopGameClicked() {
+		GameActions.stop();
+	},
 	_onChange() {
 		this.setState(GameStore.getState());
 	},
@@ -32,12 +36,14 @@ var Game = React.createClass({
 		return (
 			<div>
         <Score score={this.state.score}/>
+        <Time time={this.state.timeLeft}/>
         <button onClick={this.startGameClicked}>Start</button>
+        <button onClick={this.stopGameClicked}>Stop</button>
 				<Board length="10" onMoleClicked={this.onMoleClicked} slots={this.state.slots}/>
 			</div>
 		);
 	}
 });
 
-module.exports = Game; 
+module.exports = Game;
 
