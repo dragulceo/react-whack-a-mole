c11ae14fffd1bcd6f6e224ca7974ff3d6258d74d
dragulceo <dragulceo@gmail.com>
2015-03-13 03:55:14 +0200

    Added github link

--- src/scripts/components/About.js
+++ src/scripts/components/About.js
@@ -8,7 +8,7 @@ var About = React.createClass({
   render: function () {
     return (
 	    <div className="About">
-	      <p>Cool little game made with Yeoman, React Webpack</p>
+	      <p>Cool little game made with Yeoman, React Webpack <a href="https://github.com/dragulceo/react-whack-a-mole">Github</a></p>
 	    </div>
     );
   }
